3abca7213bd72e1ad8ffac3131764931ff666af7
Yana Shetko <mac@YanasMacBookPro.local>
2020-05-05 19:53:14 +0300

    first

--- components/VideoPlayPage.js
+++ components/VideoPlayPage.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import {  Video } from 'expo-av';
 
-
-
 export default class VideoPlayPage extends Component {
   render() {
     return (
